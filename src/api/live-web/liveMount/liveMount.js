1a976d2e02cd28d5fba92c7dfb027f9cc116a521
gebulin <123@qq.com>
2021-01-26 13:58:38 +0800

    直播开发

---
+++ src/api/live-web/liveMount/liveMount.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+import {url} from '@/utils/url'
+
+// 查询礼物列列表
+export function listLiveMount(query) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询礼物列详细
+export function getLiveMount(id) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount/' + id,
+    method: 'get'
+  })
+}
+
+// 新增礼物列
+export function addLiveMount(data) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改礼物列
+export function updateLiveMount(data) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除礼物列
+export function delLiveMount(id) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出礼物列
+export function exportLiveMount(query) {
+  return request({
+    url: url.platformWeb + '/admin/liveMount/export',
+    method: 'get',
+    params: query
+  })
+}

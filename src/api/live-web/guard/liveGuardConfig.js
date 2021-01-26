c290da7ce7bf69aa808499dfee0169c9ce497baa
gebulin <123@qq.com>
2021-01-26 14:43:09 +0800

    直播开发

---
+++ src/api/live-web/guard/liveGuardConfig.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+import {url} from '@/utils/url'
+
+// 查询【请填写功能名称】列表
+export function listLiveGuardConfig(query) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询【请填写功能名称】详细
+export function getLiveGuardConfig(id) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig/' + id,
+    method: 'get'
+  })
+}
+
+// 新增【请填写功能名称】
+export function addLiveGuardConfig(data) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改【请填写功能名称】
+export function updateLiveGuardConfig(data) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除【请填写功能名称】
+export function delLiveGuardConfig(id) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出【请填写功能名称】
+export function exportLiveGuardConfig(query) {
+  return request({
+    url: url.platformWeb + '/admin/liveGuardConfig/export',
+    method: 'get',
+    params: query
+  })
+}
---
+++ src/views/live-web/guard/index.vue
@@ -0,0 +1,297 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="规格" prop="specifications">
+        <el-input
+          v-model="queryParams.specifications"
+          placeholder="请输入规格"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['admin:liveGuardConfig:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['admin:liveGuardConfig:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['admin:liveGuardConfig:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['admin:liveGuardConfig:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="liveGuardConfigList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="赠送天数" align="center" prop="id" />
+      <el-table-column label="规格" align="center" prop="specifications" />
+      <el-table-column label="守护月数" align="center" prop="month" />
+      <el-table-column label="价格" align="center" prop="price" />
+      <el-table-column label="1银之守护2.星之守护" align="center" prop="type" />
+      <el-table-column label="礼物id" align="center" prop="propId" />
+      <el-table-column label="优惠价格" align="center" prop="discountPrice" />
+      <el-table-column label="赠送天数" align="center" prop="giveday" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['admin:liveGuardConfig:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['admin:liveGuardConfig:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改【请填写功能名称】对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="规格(一个月，三个月，六个月，十二个月)" prop="specifications">
+          <el-input v-model="form.specifications" placeholder="请输入规格(一个月，三个月，六个月，十二个月)" />
+        </el-form-item>
+        <el-form-item label="守护月数" prop="month">
+          <el-input v-model="form.month" placeholder="请输入守护月数" />
+        </el-form-item>
+        <el-form-item label="价格" prop="price">
+          <el-input v-model="form.price" placeholder="请输入价格" />
+        </el-form-item>
+        <el-form-item label="1银之守护2.星之守护" prop="type">
+          <el-select v-model="form.type" placeholder="请选择1银之守护2.星之守护">
+            <el-option label="请选择字典生成" value="" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="礼物id" prop="propId">
+          <el-input v-model="form.propId" placeholder="请输入礼物id" />
+        </el-form-item>
+        <el-form-item label="优惠价格" prop="discountPrice">
+          <el-input v-model="form.discountPrice" placeholder="请输入优惠价格" />
+        </el-form-item>
+        <el-form-item label="赠送天数" prop="giveday">
+          <el-input v-model="form.giveday" placeholder="请输入赠送天数" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listLiveGuardConfig, getLiveGuardConfig, delLiveGuardConfig, addLiveGuardConfig, updateLiveGuardConfig, exportLiveGuardConfig } from "@/api/live-web/guard/liveGuardConfig";
+
+export default {
+  name: "LiveGuardConfig",
+  components: {
+  },
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 【请填写功能名称】表格数据
+      liveGuardConfigList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        specifications: null,
+        month: null,
+        price: null,
+        type: null,
+        propId: null,
+        discountPrice: null,
+        giveday: null
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询【请填写功能名称】列表 */
+    getList() {
+      this.loading = true;
+      listLiveGuardConfig(this.queryParams).then(response => {
+        this.liveGuardConfigList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        specifications: null,
+        month: null,
+        price: null,
+        type: null,
+        propId: null,
+        discountPrice: null,
+        giveday: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加【请填写功能名称】";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getLiveGuardConfig(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改【请填写功能名称】";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateLiveGuardConfig(this.form).then(response => {
+              this.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addLiveGuardConfig(this.form).then(response => {
+              this.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除【请填写功能名称】编号为"' + ids + '"的数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return delLiveGuardConfig(ids);
+        }).then(() => {
+          this.getList();
+          this.msgSuccess("删除成功");
+        })
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有【请填写功能名称】数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return exportLiveGuardConfig(queryParams);
+        }).then(response => {
+          this.download(response.msg);
+        })
+    }
+  }
+};
+</script>

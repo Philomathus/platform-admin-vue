ed3291fcfd7f8fcec2bb231b3ade7fec9a322133
corbin <15313515@qq.com>
2021-01-29 15:59:30 +0800

    提交

---
+++ src/api/platform-web/game/configGameType.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+import {url} from '@/utils/url'
+
+// 查询【请填写功能名称】列表
+export function listConfigGametype(query) {
+  return request({
+    url: url.platformWeb + '/web/config-gametype/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询【请填写功能名称】详细
+export function getConfigGametype(id) {
+  return request({
+    url: url.platformWeb + '/web/config-gametype/' + id,
+    method: 'get'
+  })
+}
+
+// 新增【请填写功能名称】
+export function addConfigGametype(data) {
+  return request({
+    url: url.platformWeb + '/web/config-gametype',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改【请填写功能名称】
+export function updateConfigGametype(data) {
+  debugger;
+  return request({
+    url: url.platformWeb + '/web/config-gametype',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除【请填写功能名称】
+export function delConfigGametype(id) {
+  return request({
+    url: url.platformWeb + '/web/config-gametype/' + id,
+    method: 'delete'
+  })
+}
+
+export function getGameTypeInfo() {
+  return request({
+    url: url.platformWeb + '/web/config-gametype/listGameType',
+    method: 'get'
+
+  })
+}

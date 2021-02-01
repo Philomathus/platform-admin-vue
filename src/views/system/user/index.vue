ef6603ec7393b4897cdbe15b705ecfea1756e69f
xiaoou <18318392071@163.com>
2021-02-01 09:52:04 +0800

    谷歌验证码

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -566,8 +566,8 @@ export default {
     /** 谷歌验证码绑定按钮 */
     bind() {
       bindGoogleAuth(this.userName, this.secretKey, this.googleAuthCode).then(response => {
+        this.open = false
         this.msgSuccess('绑定成功')
-        this.open = true
       })
     },
     /** 删除按钮操作 */

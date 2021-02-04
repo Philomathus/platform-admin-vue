3dcfc78319d7d9d6845b6eed999ddd1b99cfcdf5
afei <afei920614@gmail.com>
2021-02-04 19:12:27 +0800

    默认时间

--- src/views/pay/payAgentRechargeAccountLog/index.vue
+++ src/views/pay/payAgentRechargeAccountLog/index.vue
@@ -266,7 +266,7 @@ export default {
       // 选中数组
       ids: [],
       // 日期范围
-      dateRange: [],
+      dateRange: [this.parseTime(this.getTodayStartTime()), this.parseTime(this.getTodayEndTime())],
       // 非单个禁用
       single: true,
       // 非多个禁用

5dd1fb20ddd2731843dfd4fb95064f0afa89ec7a
corbin <15313515@qq.com>
2021-02-04 10:44:25 +0800

    提交

--- src/views/report/comprehensiveStatistics/index.vue
+++ src/views/report/comprehensiveStatistics/index.vue
@@ -37,7 +37,6 @@
       <el-table-column label="金额" align="center" prop="tValue" :show-overflow-tooltip="true"/>
       <el-table-column label="类型" align="center" prop="type"/>
       <el-table-column label="时间" align="center" prop="reporttime" width="180"/>
-      </el-table-column>
     </el-table>
     <pagination v-show="total>0" :total="total" :page.sync="pageNum" :limit.sync="pageSize"/>
   </div>

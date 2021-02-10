a2a75aeea56adcab7c272c1011e88112deda7fbb
gebulin <123@qq.com>
2021-02-10 09:08:34 +0800

    直播开发

--- src/views/live-web/liveProp/index.vue
+++ src/views/live-web/liveProp/index.vue
@@ -326,6 +326,7 @@ export default {
           }else{
             this.form.animType="";
           }
+          this.form.score=0;
           if (this.form.id != null) {
             updateLiveProp(this.form).then(response => {
               this.msgSuccess("修改成功");

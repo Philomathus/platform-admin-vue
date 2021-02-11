f8ef7b394890ce1e43721b8aab6152dc34532a43
xiaoqicheng <xiaoqicheng5566@gmail.com>
2021-02-11 16:16:39 +0800

    fix

--- src/views/pay/memberWithdrawLog/index.vue
+++ src/views/pay/memberWithdrawLog/index.vue
@@ -82,7 +82,7 @@
     <el-table :stripe="true" v-loading="loading" :data="memberWithdrawLogList"
               @selection-change="handleSelectionChange"
     >
-      <el-table-column label="复制" align="center" >
+      <el-table-column label="复制" align="center">
         <template slot-scope="scope">
           <el-button
             type="primary" size="mini"
@@ -430,36 +430,37 @@ export default {
       this.handleQuery()
     },
     /** 复制按钮 */
-    handleCopy(row){
-      var status=this.statusOptions[parseInt(row.status)];
-      var textarea = document.createElement("textarea");
+    handleCopy(row) {
+      var status = this.statusOptions[parseInt(row.status)]
+      var textarea = document.createElement('textarea')
       let html = '<table><tr>'
       html += '<td>' + row.memberId + '</td>'
       html += '<td>' + row.account + '</td>'
+      html += '<td>' + row.rechargeWithdrawRate + '</td>'
       html += '<td>' + row.withdrawMoney + '</td>'
       html += '<td>' + row.bankUserName + '</td>'
-      html += '<td>' + "'" +row.bankAccount + "'" + '</td>'
+      html += '<td>' + '\'' + row.bankAccount + '\'' + '</td>'
       html += '<td>' + row.bankName + '</td>'
       html += '<td>' + status.dictLabel + '</td>'
       html += '<td>' + row.opName + '</td>'
       html += '<td>' + row.createTime + '</td>'
-      html += '<td>' + row.updateTime  + '</td>'
+      html += '<td>' + row.updateTime + '</td>'
       html += '</tr></table>'
-      textarea.value = html;
+      textarea.value = html
       this.copyData = html
       this.copy(this.copyData)
     },
-    copy(data){
-      let url = data;
-      let oInput = document.createElement('input');
-      oInput.value = url;
-      document.body.appendChild(oInput);
-      oInput.select(); // 选择对象;
-      document.execCommand("Copy"); // 执行浏览器复制命令
+    copy(data) {
+      let url = data
+      let oInput = document.createElement('input')
+      oInput.value = url
+      document.body.appendChild(oInput)
+      oInput.select() // 选择对象;
+      document.execCommand('Copy') // 执行浏览器复制命令
       this.$message({
         message: '复制成功',
         type: 'success'
-      });
+      })
       oInput.remove()
     },
     // 多选框选中数据

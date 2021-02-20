45ee0750698fb0113738391f48e335b9e43aabb8
xiaoou <18318392071@163.com>
2021-02-20 17:26:26 +0800

    游戏投注列表跳转

---
+++ src/api/platform-web/report/gameBetJump.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+import {url} from '@/utils/url'
+
+// 查询【请填写功能名称】列表
+export function list(query) {
+  return request({
+    url: url.platformWeb + '/admin/reportPlamGameschilds/list',
+    method: 'get',
+    params: query
+  })
+}
+
--- src/views/report/gameBetJump/index.vue
+++ src/views/report/gameBetJump/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="app-container">
-    <el-button type="primary" @click="copy1">总投注金额: {{ this.data.countBetMoney || 0 }}</el-button>
-    <el-button type="success" @click="copy2">总投注人数: {{ this.data.countBetPeople || 0 }}</el-button>
     <el-form :model="queryParams" ref="queryForm" style="margin-top: 10px" :inline="true" label-width="68px" v-show="showSearch">
       <el-form-item label="日期选择" prop="begindate">
         <el-date-picker v-model="queryParams.begindate" format="yyyy-MM-dd" value-format="yyyy-MM-dd"
@@ -19,22 +17,10 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-          v-hasPermi="['admin:reportPlamGames:export']"
-        >导出
-        </el-button>
-      </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
     <el-table v-loading="loading"
@@ -44,9 +30,6 @@
     >
       <el-table-column label="平台编号" align="center" prop="gameagent" :show-overflow-tooltip="true"/>
       <el-table-column label="名称-详情" align="center" prop="gameplame">
-      <template slot-scope="scope">
-        <a style="color: #00afff" @click="jump(scope.row.begindate,scope.row.gameplame)">{{scope.row.gameplame}}</a>
-      </template>
       </el-table-column>
       <el-table-column label="投注人数" align="center" prop="gamepepole"/>
       <el-table-column label="投注比数" align="center" prop="gametouzhu"/>
@@ -62,7 +45,7 @@
 </template>
 
 <script>
-import { list, count, liststorage,exportReportPlamGames } from '@/api/platform-web/report/gameBet'
+import { list } from '@/api/platform-web/report/gameBetJump'
 import { getYesterDate } from '@/utils/dateUtils'
 
 export default {
@@ -75,25 +58,28 @@ export default {
       total: 0,
       // 显示搜索条件
       showSearch: true,
-      // countBetMoney:null,
-      // countBetPeople:null,
       // 表格数据
       list: [],
       data: {},
       // 查询参数
       queryParams: {
-        begindate: this.parseTime(getYesterDate(), '{y}-{m}-{d}'),
+        begindate: null,
         gameplame: null
       }
     }
   },
   created() {
-    this.getliststorage()
+    var begindate = this.$route.query.begindate;
+    var gameplame = this.$route.query.gameplame;
+    if(gameplame!=null) {
+      this.queryParams.gameplame = gameplame
+    }
+    if(begindate!=null) {
+      this.queryParams.begindate = begindate
+    }
     this.getList()
-    this.count()
   },
   methods: {
-
     getList() {
       this.loading = true
       list(this.queryParams).then(response => {
@@ -102,57 +88,12 @@ export default {
         this.loading = false
       })
     },
-    getliststorage() {
-      this.loading = true
-      liststorage(this.queryParams).then(response => {
-      })
-    },
-    //复制
-    copy1() {
-      this.copyCommand(this.data.countBetMoney)
-    },
-    copy2() {
-      this.copyCommand(this.data.countBetPeople)
-    },
-    jump(begindate,gameplame){
-      this.$router.push({path: '/report/gameBetJump',query: { begindate: begindate, gameplame:gameplame}})
-    },
-    //统计
-    count() {
-      this.loading = true
-      count(this.queryParams).then(response => {
-        if (response.data) {
-          this.data = response.data
-        }
-        this.loading = false
-      })
-    },
     /** 搜索按钮操作 */
     handleQuery() {
-
       this.pageNum = 1
-      this.getliststorage()
       this.getList()
-      this.count()
-    },
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm('queryForm')
-      this.handleQuery()
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      const queryParams = this.queryParams
-      this.$confirm('是否确认导出所有会员数据项?', '警告', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(function() {
-        return exportReportPlamGames(queryParams)
-      }).then(response => {
-        this.download(response.msg)
-      })
     }
+
   }
 }
 </script>

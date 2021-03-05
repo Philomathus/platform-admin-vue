6ef6a909df8233beb6cc4aa95b422c56f32dfb23
xiaoou <18318392071@163.com>
2021-03-05 10:53:06 +0800

    日期快捷

--- src/views/report/rechargeStatistics/index.vue
+++ src/views/report/rechargeStatistics/index.vue
@@ -110,6 +110,14 @@ export default {
             picker.$emit('pick', [start, end]);
           }
         }, {
+          text: '最近半个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 15);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
           text: '最近一个月',
           onClick(picker) {
             const end = new Date();

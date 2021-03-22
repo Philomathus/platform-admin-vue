24458bc31a0cbfb702284dde9ad23d3fb8d6399b
xiaoou <18318392071@163.com>
2021-03-22 13:48:45 +0800

    操作人搜索

--- src/views/pay/memberRechargeLog/index.vue
+++ src/views/pay/memberRechargeLog/index.vue
@@ -62,6 +62,15 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
+      <el-form-item prop="opName" style="width: 120px;">
+        <el-input
+          v-model="queryParams.opName"
+          placeholder="操作人"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>

e713ce2cf8e2fd4f03ff43ef766b345378f4a322
xiaoou <18318392071@163.com>
2021-04-08 19:29:26 +0800

    主播时长

--- src/views/live-web/liveHostWageNote/index.vue
+++ src/views/live-web/liveHostWageNote/index.vue
@@ -188,7 +188,6 @@ export default {
         this.liveHostWageNoteList.forEach(value => {
           value.totalsettle = (parseFloat(value.livetimejiesuan)  + parseFloat(value.liwujiesuan) + parseFloat(value.costQianliu)).toFixed(2)
         })
-        // this.liveHostWageNoteList.totalsettle = this.liveHostWageNoteList.livetimejiesuan + response.rows.liwujiesuan + response.rows.costQianliu
         this.total = response.total
         this.loading = false
       })

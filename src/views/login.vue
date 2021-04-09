15d1233adbdc865492ef1cd1f366370f55928797
qicheng <xiaoqicheng5566@gmail.com>
2021-04-09 13:18:33 +0800

    fix

--- src/views/login.vue
+++ src/views/login.vue
@@ -9,7 +9,7 @@
           auto-complete="off"
           placeholder="账号"
         >
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon"/>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
@@ -19,7 +19,7 @@
           auto-complete="off"
           placeholder="密码"
         >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon"/>
         </el-input>
       </el-form-item>
       <el-form-item prop="googleAuthCode">
@@ -28,8 +28,13 @@
           auto-complete="off"
           placeholder="google验证码"
           @keyup.enter.native="handleLogin"
+          type="number"
+          @input="loginForm.googleAuthCode.length>6
+            ? loginForm.googleAuthCode = loginForm.googleAuthCode.slice(0,6)
+            : loginForm.googleAuthCode"
+          class="no-number"
         >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon"/>
         </el-input>
       </el-form-item>
       <el-form-item style="width:100%;">
@@ -56,32 +61,32 @@
 import { encrypt, decrypt } from '@/utils/jsencrypt'
 
 export default {
-  name: "Login",
+  name: 'Login',
   data() {
     return {
       loginForm: {
-        username: "",
-        password: "",
-        googleAuthCode: "",
-        uuid: ""
+        username: '',
+        password: '',
+        googleAuthCode: '',
+        uuid: ''
       },
       loginRules: {
         username: [
-          { required: true, trigger: "blur", message: "用户名不能为空" }
+          { required: true, trigger: 'blur', message: '用户名不能为空' }
         ],
         password: [
-          { required: true, trigger: "blur", message: "密码不能为空" }
+          { required: true, trigger: 'blur', message: '密码不能为空' }
         ],
-        googleAuthCode: [{ required: true, trigger: "change", message: "google验证码不能为空" }]
+        googleAuthCode: [{ required: true, trigger: 'change', message: 'google验证码不能为空' }]
       },
       loading: false,
       redirect: undefined
-    };
+    }
   },
   watch: {
     $route: {
       handler: function(route) {
-        this.redirect = route.query && route.query.redirect;
+        this.redirect = route.query && route.query.redirect
       },
       immediate: true
     }
@@ -90,17 +95,18 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.loading = true;
-          this.$store.dispatch("Login", this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
+          this.loading = true
+          this.$store.dispatch('Login', this.loginForm).then(() => {
+            this.$router.push({ path: this.redirect || '/' }).catch(() => {
+            })
           }).catch(() => {
-            this.loading = false;
-          });
+            this.loading = false
+          })
         }
-      });
+      })
     }
   }
-};
+}
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
@@ -112,6 +118,7 @@ export default {
   background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
 }
+
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
@@ -123,32 +130,39 @@ export default {
   background: #ffffff;
   width: 400px;
   padding: 25px 25px 5px 25px;
+
   .el-input {
     height: 38px;
+
     input {
       height: 38px;
     }
   }
+
   .input-icon {
     height: 39px;
     width: 14px;
     margin-left: 2px;
   }
 }
+
 .login-tip {
   font-size: 13px;
   text-align: center;
   color: #bfbfbf;
 }
+
 .login-code {
   width: 33%;
   height: 38px;
   float: right;
+
   img {
     cursor: pointer;
     vertical-align: middle;
   }
 }
+
 .el-login-footer {
   height: 40px;
   line-height: 40px;
@@ -161,6 +175,7 @@ export default {
   font-size: 12px;
   letter-spacing: 1px;
 }
+
 .login-code-img {
   height: 38px;
 }

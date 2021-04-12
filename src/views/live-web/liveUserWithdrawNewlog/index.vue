4d7eca3b82c992fd9e920bf3d847c61991b3248c
afei <afei920614@gmail.com>
2021-04-12 18:39:38 +0800

    tixian

--- src/views/live-web/liveUserWithdrawNewlog/index.vue
+++ src/views/live-web/liveUserWithdrawNewlog/index.vue
@@ -221,7 +221,7 @@
       <el-table-column label="主播ID" align="center" prop="userId" min-width="80px" />
       <el-table-column label="家族ID" align="center" prop="familyId">
         <template v-slot="{row}">
-          <a style="color: #00afff" @click="familyShow(row.familyId,row.createTime)">{{ row.familyId }}</a>
+          <a style="color: #00afff" @click="familyShow(row.familyId,row.createTime,row.type,row.userId)">{{ row.familyId }}</a>
         </template>
       </el-table-column>
       <el-table-column label="主播昵称" align="center" prop="nickName" min-width="120px" />
@@ -519,12 +519,14 @@ export default {
       });
       return string;
     },
-    familyShow(familyId,createTime) {
+    familyShow(familyId,createTime,type,userId) {
       this.$router.push({
         path: '/live/live/liveHostWageNoteJump',
         query: {
           familyId: familyId,
           createTime:createTime,
+          hostId:userId,
+          //type:type,
           // settlementRate: this.queryParams.settlementRate,
           //selectDate: this.queryParams.selectDate
         }

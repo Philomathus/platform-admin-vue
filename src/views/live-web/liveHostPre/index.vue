9eaa4478455ca4b391cdeb336ebb7f10cf48232a
xiaoou <18318392071@163.com>
2021-04-14 15:09:46 +0800

    fix

--- src/views/live-web/liveHostPre/index.vue
+++ src/views/live-web/liveHostPre/index.vue
@@ -325,6 +325,8 @@
           </span>
         </template>
       </el-table-column>
+      <el-table-column label="备注" align="center" prop="remark" min-width="120"/>
+      <el-table-column label="创建时间" align="center" prop="createTime" min-width="200"/>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right" width="200">
         <template slot-scope="scope">
           <el-button

556f2cad6c252409c451a9957b17ed78be7fe46b
xiaoou <18318392071@163.com>
2021-04-16 13:08:43 +0800

    fix

--- src/views/pay/payAgentRechargeLog/index.vue
+++ src/views/pay/payAgentRechargeLog/index.vue
@@ -341,15 +341,16 @@ export default {
     },
     /** 导出按钮操作 */
     handleExport() {
-      const queryParams = this.queryParams;
-      this.$confirm('是否确认导出所有代充信息日志数据项?', "警告", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
+      const queryParams = this.queryParams
+      this.$confirm('确认处理Excel并下载，数据量大的时候会延迟，请耐心等待...', '警告', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning'
       }).then(function() {
-        return exportPayAgentRechargeLog(queryParams);
+        return exportPayAgentRechargeLog(queryParams)
       }).then(response => {
-        this.download(response.msg);
+        this.downloadExcel(response, '公司入款')
+      }).catch(() => {
       })
     }
   }

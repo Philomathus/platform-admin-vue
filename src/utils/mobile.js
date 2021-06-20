34135e8a9abf9ed642c78d224f34d8232f6b29be
axing <1792836541@qq.com>
2021-06-20 12:31:23 +0400

    手机号没有报错

--- src/utils/mobile.js
+++ src/utils/mobile.js
@@ -1,6 +1,6 @@
 //手机号隐藏中间四位
 export const hideKMobile = function(mobile) {
-  if (mobile.length === 11) {
+  if (mobile && mobile.length === 11) {
     return mobile.substr(0,3) + "****" + mobile.substr(7,4)
   }else {
     return mobile

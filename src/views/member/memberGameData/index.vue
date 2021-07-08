fa19a35643243001ca5961c84aefd4fabf8a8a90
xiaomeng <xiaomeng123>
2021-07-08 16:30:49 +0800

    // add 平台列表

--- src/views/member/memberGameData/index.vue
+++ src/views/member/memberGameData/index.vue
@@ -64,7 +64,7 @@
       <el-table-column label="游戏局号" align="center"  min-width="180px" :show-overflow-tooltip="true" prop="gameId">
         <template v-slot="{row}">
             <div v-if="row.platformId == 1 || row.platformId == 15 || row.platformId == 17">
-              <a style="color: #00afff"  @click="openAgPlaypDetail(row)">{{ row.gameId }}</a>
+              <a style="color: #00afff"  @click="handleRecord(row)">{{ row.gameId }}</a>
             </div>
             <div v-else-if="row.platformId == 14">
               <a style="color: #00afff"  @click="openRecordLink(row)">{{ row.gameId }}</a>

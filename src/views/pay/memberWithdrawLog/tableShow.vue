b4efa9aae6bb76a0eb6165e34ba9bdfb90f15017
xiaoou <18318392071@163.com>
2021-07-08 15:25:36 +0400

    查看完整手机号

--- src/views/pay/memberWithdrawLog/tableShow.vue
+++ src/views/pay/memberWithdrawLog/tableShow.vue
@@ -8,14 +8,16 @@
       <div class="mount" style="width: 12%">
         <div class="font">会员Id</div>
         <div class="font">注册时间</div>
-        <div class="font">电话号码</div>
+<!--        <div class="font">电话号码</div>-->
+        <div class="font">用户类型</div>
         <div class="font">会员打码</div>
         <div class="font">会员IP</div>
       </div>
       <div class="mount" style="width: 28%">
         <div class="font">{{ data.会员编号 }}</div>
         <div class="font">{{ data.会员注册时间 }}</div>
-        <div class="font" @click="showPhone" style="background-color: #cccc77">{{ data.会员名称 }}</div>
+<!--        <div class="font" @click="showPhone" style="background-color: #cccc77">{{ data.会员名称 }}</div>-->
+        <div class="font">{{ data.用户类型 }}</div>
         <div class="font">{{ data.会员打码 }}</div>
         <div class="font">{{ data.登陆IP }}</div>
       </div>

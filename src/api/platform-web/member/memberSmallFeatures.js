5ad5c5af79402a38ec5c5d13ed398d62ac7d3148
xiaoou <18318392071@163.com>
2021-08-25 10:28:34 +0400

    会员小功能回显手机号

--- src/api/platform-web/member/memberSmallFeatures.js
+++ src/api/platform-web/member/memberSmallFeatures.js
@@ -1,10 +1,19 @@
 import request from '@/utils/request'
 import {url} from '@/utils/url'
 
-//会员小功能之批量根据手机号更新密码
+//会员小功能之批量手机号更新密码
 export function updatePhones(query) {
   return request({
     url: url.platformWeb + '/member/memberInfo/memberSmallFeatures',
+    method: 'post',
+    params: query
+  })
+}
+
+//会员小功能之批量会员ID查询手机号
+export function queryPhones(query) {
+  return request({
+    url: url.platformWeb + '/member/memberInfo/queryPhones',
     method: 'post',
     params: query
   })

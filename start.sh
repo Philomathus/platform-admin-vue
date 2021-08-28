3232a4be63b840934f94ec439c33b6bc9f5a80ce
qicheng <qicheng7777@gmail.com>
2021-08-28 16:43:22 +0400

    fix

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p `pwd`/conf `pwd`/logs
-docker run --restart=always -d --name nginx \
+mkdir -p `pwd`/logs
+docker run --restart=always -d --name platform-admin-vue \
   --expose 80 -p 80:80
 	-v `pwd`/logs/:/etc/nginx/logs/ \
 	\
 	-e GZIP=on \
-	77tv/nginx:1.18.0
+	77tv/platform-admin-vue:latest

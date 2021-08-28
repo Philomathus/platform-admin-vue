abf3b0ae1c7844e0aad6d491c1e7800502978378
xiaomeng <xiaomeng123>
2021-08-28 17:58:11 +0800

    // add 平台列表

--- src/views/member/memberGameData/record.vue
+++ src/views/member/memberGameData/record.vue
@@ -47,7 +47,7 @@
     <el-dialog title="游戏对局日志" :visible.sync="detailOpen" width="1500px" style="max-height:100%;overflow-y: scroll;"
                append-to-body>
       <div v-loading="loading" :style="'height:'+ height">
-        <iframe :src="detailLink" frameborder="no" style="width: 100%;height: 600px" scrolling="auto" />
+        <iframe :src="detailLink" frameborder="no" style="width: 100%;height: 650px" scrolling="auto" />
       </div>
     </el-dialog>
   </div>

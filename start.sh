9c342cf1286518f90ecccd59c14ce0814b5b52e0
qicheng <qicheng7777@gmail.com>
2021-09-07 08:31:37 +0400

    fix

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p `pwd`/logs
-docker run --restart=always -d --name platform-admin-vue \
-  --expose 80 -p 80:80 \
-	-v `pwd`/logs/:/etc/nginx/logs/ \
-	\
-	-e GZIP=on \
-	77tv/platform-admin-vue:latest
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker kill --signal=SIGQUIT platform-admin-vue
-
-docker rm -f platform-admin-vue

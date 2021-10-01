cced3207b732410fbddd7c9a9d1a9269a91b50b0
xiaoou <18318392071@163.com>
2021-10-01 08:39:28 +0400

    fix

--- src/views/member/memberVideo/index.vue
+++ src/views/member/memberVideo/index.vue
@@ -16,7 +16,7 @@
             :on-exceed="handleExceed"
             :file-list="fileList">
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            <div slot="tip" class="el-upload__tip">只能上传MP4格式文件，且不超过300MB</div>
           </el-upload>
         </el-card>
       </el-col>

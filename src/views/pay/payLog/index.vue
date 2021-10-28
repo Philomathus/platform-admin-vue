6cfec645c317af2413b5284f68287743d1ca26ee
xiaoou <18318392071@163.com>
2021-10-28 10:05:43 +0400

    fix

--- src/views/pay/payLog/index.vue
+++ src/views/pay/payLog/index.vue
@@ -60,7 +60,6 @@
           <el-option label="网络连接失败,下单报错" value="网络连接失败,下单报错"></el-option>
           <el-option label="网络连接失败,下单返回空值" value="网络连接失败,下单返回空值"></el-option>
           <el-option label="首次下单超时,二次下单成功" value="首次下单超时,二次下单成功"></el-option>
-          <el-option label="首次下单空值,二次下单成功" value="首次下单空值,二次下单成功"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -113,7 +112,8 @@
               <div>网络连接失败,下单返回空值:指下单时发送请求返回的结果为空</div>
               <div>对于以上三种情况,为三方支付平台的网络原因</div>
               <div>如果某个支付通道失败订单占所有订单的比例过多,请在三方Telegram群里联系支付平台运营人员</div>
-              <div>对于下单超时和下单返回空值两种失败原因订单,有二次下单机制</div>
+              <div>对于下单超时订单,有二次下单机制</div>
+              <div>对于(首次下单超时,二次下单成功)失败原因订单,请选择状态为'成功'再查询</div>
             </div>
           </el-tooltip>
         </template>

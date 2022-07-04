e478c4ab50db6afb3c0b187113531cc1b723ed88
Rajesh <rkyadavsoft@gmail.com>
2022-07-04 14:01:28 +0800

    fixed updated code

--- src/views/game/gameInfo/index.vue
+++ src/views/game/gameInfo/index.vue
@@ -123,20 +123,29 @@
 
 
     <!-- 添加或修改游戏信息对话框 -->
-    <el-dialog :close-on-click-modal="false" :title="title" :visible.sync="open" width="700px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="110px">
+    <el-dialog :close-on-click-modal="false" :title="title" :visible.sync="open" width="600px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
         <el-form-item label="游戏名称" prop="name">
           <el-input v-model="form.name" placeholder="请输入游戏名称"/>
         </el-form-item>
         <el-form-item label="排序号" prop="indexs">
           <el-input v-model="form.indexs" placeholder="请输入排序号"/>
         </el-form-item>
-        <el-form-item label="竖版新版图标" prop="icon">
-          <imageUpload v-model="form.icon" path="gameInfo"/>
-        </el-form-item>
-        <el-form-item label="竖版图标" prop="editionIcon">
-          <imageUpload v-model="form.editionIcon" path="gameInfo"/>
-        </el-form-item>
+
+        <div class="el-row">
+          <div class="el-col-lg-11">
+            <el-form-item label="竖版新版图标" prop="icon">
+              <imageUpload v-model="form.icon" path="gameInfo"/>
+            </el-form-item>
+          </div>
+          <div class="el-col-lg-10">
+            <el-form-item label="竖版图标" prop="editionIcon">
+              <imageUpload v-model="form.editionIcon" path="gameInfo"/>
+            </el-form-item>
+          </div>
+        </div>
+
+
         <el-form-item label="游戏码" prop="kindId">
           <el-input v-model="form.kindId" placeholder="请输入游戏码" style="width: 240px"/>
         </el-form-item>

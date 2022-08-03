fb2000e51c481746629bfcf73b9b95236febde29
Rajesh <rkyadavsoft@gmail.com>
2022-08-03 19:00:04 +0800

    adjusted

--- src/views/lottery/wheelPoolHistory/index.vue
+++ src/views/lottery/wheelPoolHistory/index.vue
@@ -28,12 +28,13 @@
     <!-- 轮式台球历史表  wheel pool history table-->
     <el-table stripe v-loading="loading" :data="wheelPoolHistoryList" class="el-table--border">
       <el-table-column label="主键" align="center" prop="id" />
-      <el-table-column label="会员ID" align="center" prop="memberId" />
+      <el-table-column label="会员ID" align="center" prop="memberId" min-width="90"/>
       <el-table-column label="昵称" align="center" prop="nickName" />
       <el-table-column label="中奖ID" align="center" prop="winId" />
       <el-table-column label="中奖金额" align="center" prop="prize" />
       <el-table-column label="是否首次中奖" align="center" prop="first" />
-      <el-table-column label="状态" align="center" prop="status"><!-- 0 未中奖 1 已中奖 2 已放弃-->
+
+      <el-table-column label="状态" align="center" prop="status">  <!-- 0 未中奖 1 已中奖 2 已放弃-->
         <template slot-scope="scope">
           <span :style="{color: (status = wheelStatus[parseInt(scope.row.status)]).color}">
             {{ status.dictLabel }}
@@ -44,7 +45,7 @@
       <el-table-column label="抽奖排序" align="center" prop="drawType" min-width="140" />
       <el-table-column label="已抽位置" align="center" prop="position" min-width="170" />
       <el-table-column label="创建时间" align="center" prop="createTime"  min-width="110" />
-      <el-table-column label="更新时间" align="center" prop="updateTime"  min-width="110" />
+      <el-table-column label="更新时间" align="center" prop="updateTime"  min-width="120" />
     </el-table>
 
     <pagination

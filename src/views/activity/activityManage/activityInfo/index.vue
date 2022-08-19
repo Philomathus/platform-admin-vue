f5502de64c21d44b9ae2d4c44ffe81f2f984b20a
Rajesh <rkyadavsoft@gmail.com>
2022-08-19 18:35:58 +0800

    fix

--- src/views/activity/activityManage/activityInfo/index.vue
+++ src/views/activity/activityManage/activityInfo/index.vue
@@ -129,6 +129,7 @@
             v-hasPermi="['admin:activityInfo:edit']">修改
           </el-button>
           <el-button
+            style="color: #d24141"
             size="mini"
             type="text"
             icon="el-icon-delete"

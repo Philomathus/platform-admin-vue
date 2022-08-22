a9c2bea71d48a857025b64ece8a5645f54b597f6
Rajesh <rkyadavsoft@gmail.com>
2022-08-22 18:24:17 +0800

    1

--- src/views/member/memberInfo/index.vue
+++ src/views/member/memberInfo/index.vue
@@ -195,7 +195,7 @@
         </template>
       </el-table-column>
       <el-table-column label="登录备注" align="center" prop="email" :show-overflow-tooltip="true" min-width="130px"/>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right">
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" fixed="right"  min-width="100">
         <template slot-scope="scope">
           <el-button
             type="primary"

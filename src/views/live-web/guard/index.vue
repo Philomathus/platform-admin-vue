a4a891883d48ff0714f930c610dc554c166e4394
Rajesh <rkyadavsoft@gmail.com>
2022-08-24 17:52:23 +0800

    fix

--- src/views/live-web/guard/index.vue
+++ src/views/live-web/guard/index.vue
@@ -86,6 +86,7 @@
           >修改
           </el-button>
           <el-button
+            style="color: #FF5722"
             size="mini"
             type="text"
             icon="el-icon-delete"

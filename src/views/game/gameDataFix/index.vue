feca8a2bdc6be29482bce2d28f39554e2d866ffe
Rajesh <rkyadavsoft@gmail.com>
2022-08-26 15:44:43 +0800

    fix

--- src/views/game/gameDataFix/index.vue
+++ src/views/game/gameDataFix/index.vue
@@ -124,6 +124,7 @@
             v-hasPermi="['game:memberGameDatafix:edit']"
           >修改</el-button>
           <el-button
+            style="color: #FF5722"
             size="mini"
             type="text"
             icon="el-icon-delete"

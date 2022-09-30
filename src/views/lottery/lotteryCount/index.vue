0ed2c667d1a9e7dc94af94b6f3ecf80063eaeaba
Rajesh <rkyadavsoft@gmail.com>
2022-09-30 18:05:21 +0800

    added search lottery count

--- src/views/lottery/lotteryCount/index.vue
+++ src/views/lottery/lotteryCount/index.vue
@@ -1,11 +1,60 @@
 <template>
   <div class="app-container">
+    <!--    search form 搜索表格 -->
+    <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px" style="margin-top: 15px;">
+
+      <el-form-item label="会员ID" prop="puserId">
+        <el-input
+          v-model="queryParams.pUserId"
+          placeholder="请输入会员ID"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"/>
+      </el-form-item>
+      <el-form-item label="下注内容" prop="betInfo">
+        <el-input
+          v-model="queryParams.betInfo"
+          placeholder="下注内容"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"/>
+      </el-form-item>
+
+      <el-form-item label="期数" prop="issue">
+        <el-input
+          v-model="queryParams.issue"
+          placeholder="期数"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"/>
+      </el-form-item>
+
+      <el-form-item prop="lotteryName">
+        <el-select
+          v-model="queryParams.lotteryName"
+          placeholder="请选择彩票名称"
+          clearable
+          size="small"
+          style="width: 240px">
+          <el-option
+            v-for="dict in lotteryNameOptions"
+            :key="dict.name"
+            :label="dict.name"
+            :value="dict.name"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
     <!--member lottery count table-->
     <el-table stripe v-loading="loading"  :data="lotteryCountList" class="el-table--border">
       <el-table-column label="主键" prop="id"  align="center" min-width="120"/>
-      <el-table-column label="彩票ID" prop="lotteryId" align="center"  min-width="120"/>
+      <el-table-column label="彩票名称" prop="lotteryName" align="center"  min-width="120"/>
       <el-table-column label="会员ID" prop="puserId" align="center" min-width="120"/>
-<!--      <el-table-column label="agent" prop="agent" align="center" min-width="120"/>-->
       <el-table-column label="期数"  prop="issue" align="center"  min-width="120"/>
       <el-table-column label="下注内容" prop="betInfo" align="center"   min-width="120"/>
       <el-table-column label="下注金额" prop="chip" align="center"   min-width="120"/>
@@ -25,6 +74,7 @@
 
 <script>
 import {listLotteryCount} from "@/api/platform-web/lottery/lotteryCount";
+import {listLotteryInfo} from "@/api/platform-web/lottery/lotteryInfo";
 
 export default {
   name: "index",
@@ -33,21 +83,26 @@ export default {
       // 遮罩层 - loading layer
       loading: true,
       lotteryCountList: [],
+      lotteryNameOptions : [],
 
       /** 总条数 total number*/
       total: 0,
+      /**显示搜索条件  Show search criteria */
+      showSearch: true,
 
       queryParams: {
          id:null,
          agent:"",
-         lotteryId : null,
-         puserId: "",
+         lotteryName : "",
+         name : "",
+         lotteryId:"",
+         pUserId: "",
          issue : "",
          betInfo : "",
          chip : "",
          ip : "",
          pageNum: 1,
-         pageSize: 20,
+         pageSize: 15
       },
 
     };
@@ -55,8 +110,10 @@ export default {
 
   created() {
     this.getList();
+    this.getLotteryNameList();
   },
   methods: {
+
     /** 获取轮池的所有列表 get all list of wheel pool*/
     getList() {
       this.loading = true;
@@ -66,6 +123,25 @@ export default {
         this.loading = false;
       });
     },
+
+    getLotteryNameList() {
+      listLotteryInfo().then(response => {
+        this.lotteryNameOptions = response.rows
+      })
+    },
+
+    /** 搜索按钮操作 handle search query */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+
+    /** 重置按钮操作  Reset button action*/
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+
   }
 
 }

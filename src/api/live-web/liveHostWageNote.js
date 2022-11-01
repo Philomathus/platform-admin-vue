a40b24f95363fe86c6c2d61c3bd0d000da2d6124
rajesh <rkyadavsoft@gmail.com>
2022-11-01 19:28:38 +0800

    fix

--- src/api/live-web/liveHostWageNote.js
+++ src/api/live-web/liveHostWageNote.js
@@ -53,7 +53,7 @@ export function exportHostWageNote(query) {
 // 查询主播统计时长明细列表
 export function listHostWageNoteDaysPage(query) {
   return request({
-    url: url.platformWeb + '/admin/liveHostWageDay/lists',
+    url: url.platformWeb + '/admin/liveHostWageDay/list',
     method: 'get',
     params: query
   })

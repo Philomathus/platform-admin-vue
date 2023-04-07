c067b86babd7187d48a56b4368a338e70af357fb
mark <luis.wbdriges@gmail.com>
2023-04-07 12:45:07 +0800

    Added notification switch for deposit and withdraw

---
+++ src/components/SwitchDeposit/index.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <el-switch  v-model="switchValue"
+              style="padding-top: 13px"
+              active-value="ON"
+              inactive-value="OFF"
+              @change="turnOnNotification"
+
+  />
+</template>
+
+<script>
+import depositAudio from '../../assets/audio/notif_sound_deposit.wav';
+const SW_ON = "ON";
+const SW_OFF = "OFF";
+
+
+export default {
+  data(){
+    return {
+      switchValue: SW_ON,
+      depositAudio: null,
+      hasDeposit: false
+    };
+  },
+  mounted(){
+    this.depositAudio  = new Audio(depositAudio)
+  },
+  methods: {
+    turnOnNotification(){
+      this.hasDeposit = true;
+      if ( this.switchValue === SW_ON && this.hasDeposit ){
+        //Everytime someone do deposit, play sound...
+
+
+        this.depositAudio.play();
+        console.log( "Deposit Notification Sound is Playing ... " );
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SwitchWithdraw/index.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <el-switch  v-model="switchValue"
+              style="padding-top: 13px"
+              active-value="ON"
+              inactive-value="OFF"
+              @change="turnOnNotification"
+
+  />
+</template>
+
+<script>
+import withdrawAudio from '../../assets/audio/notif_sound_withdraw.wav';
+
+const SW_ON = "ON";
+const SW_OFF = "OFF";
+
+export default {
+  data(){
+    return {
+      switchValue: SW_ON,
+      withdrawAudio: null,
+      hasWithdraw: false
+    };
+  },
+  mounted(){
+    this.withdrawAudio = new Audio(withdrawAudio)
+  },
+  methods: {
+    turnOnNotification(){
+      this.hasWithdraw = true;
+      if ( this.switchValue === SW_ON && this.hasWithdraw ){
+        //Everytime someone do deposit, play sound...
+
+
+
+        this.withdrawAudio.play();
+        console.log( "Withdraw Notification Sound is Playing ... " );
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>

213aba00e70d416d154ea82b72b6578bc68080d5
mark <luis.wbdriges@gmail.com>
2023-04-07 19:49:56 +0800

    Modify Business Logic for Deposit and Withdraw Notification

--- src/components/SwitchDeposit/index.vue
+++ src/components/SwitchDeposit/index.vue
@@ -1,40 +1,92 @@
 <template>
 
-  <el-switch  v-model="switchValue"
-              style="padding-top: 13px"
-              active-value="ON"
-              inactive-value="OFF"
-              @change="turnOnNotification"
-
+  <el-switch v-model="notifyOnDeposit"
+             style="padding-top: 13px"
+             @change="scheduleReminder"
   />
+
 </template>
 
 <script>
 import depositAudio from '../../assets/audio/notif_sound_deposit.wav';
-const SW_ON = "ON";
-const SW_OFF = "OFF";
+import { listCount as memberPayJourListCount } from '@/api/platform-web/pay/memberPayJour';
+import { listCount as memberRechargeLogListCount } from '@/api/platform-web/pay/memberRechargeLog';
+import { listCount as payUsdtRechargeListCount } from '@/api/platform-web/pay/payUsdtRecharge';
+import { countMoney as payAgentRechargeLogListCount } from '@/api/platform-web/pay/payAgentRechargeLog';
 
 
 export default {
   data(){
     return {
-      switchValue: SW_ON,
-      depositAudio: null,
-      hasDeposit: false
+      notifyOnDeposit: true,
+      totals: {
+        memberPayJourListCount: 0,
+        memberRechargeLogListCount: 0,
+        payUsdtRechargeListCount: 0,
+        payAgentRechargeLogListCount: 0
+      },
+      audio: new Audio( depositAudio ),
+      query: {
+        selectDate: [ this.parseTime( this.getTodayStartTime() ), this.parseTime( this.getTodayEndTime() ) ]
+      },
+      reminderInterval: null
     };
   },
   mounted(){
-    this.depositAudio  = new Audio(depositAudio)
+    memberPayJourListCount( this.query ).then( res => {
+      this.totals.memberPayJourListCount = res.total;
+    })
+    memberRechargeLogListCount( this.query ).then( res => {
+      this.totals.memberRechargeLogListCount = res.total;
+    })
+    payUsdtRechargeListCount( this.query ).then( res => {
+      this.totals.payUsdtRechargeListCount = res.total;
+    })
+    payAgentRechargeLogListCount( this.query ).then( res => {
+      this.totals.payAgentRechargeLogListCount = res.data.countNumber;
+    })
+
+    this.scheduleReminder();
   },
   methods: {
-    turnOnNotification(){
-      this.hasDeposit = true;
-      if ( this.switchValue === SW_ON && this.hasDeposit ){
-        //Everytime someone do deposit, play sound...
-
-
-        this.depositAudio.play();
-        console.log( "Deposit Notification Sound is Playing ... " );
+    scheduleReminder() {
+      if( this.notifyOnDeposit ) {
+        let play = false;
+        this.reminderInterval = setInterval(() => {
+          memberPayJourListCount( this.query ).then( res => {
+              if( res.total > this.totals.memberPayJourListCount ) {
+                this.totals.memberPayJourListCount = res.total;
+                play = true;
+              }
+            }
+          );
+          memberRechargeLogListCount( this.query ).then( res => {
+              if( res.total > this.totals.memberRechargeLogListCount ) {
+                this.totals.memberRechargeLogListCount = res.total;
+                play = true;
+              }
+            }
+          );
+          payUsdtRechargeListCount( this.query ).then( res => {
+              if( res.total > this.totals.payUsdtRechargeListCount ) {
+                this.totals.payUsdtRechargeListCount = res.total;
+                play = true;
+              }
+            }
+          );
+          payAgentRechargeLogListCount( this.query ).then( res => {
+              if( res.data.countNumber > this.totals.payAgentRechargeLogListCount ) {
+                this.totals.payAgentRechargeLogListCount = res.data.countNumber ;
+                play = true;
+              }
+            }
+          );
+          if ( play ) {
+            this.audio.play();
+          }
+        }, 3000);
+      } else {
+        clearInterval( this.reminderInterval );
       }
     }
   }

adca099ff957359f283f98bcf82d2c09a2d60974
rajesh <rkyadavsoft@gmail.com>
2023-05-28 21:43:32 +0800

    updated

--- src/views/report/gameBetJump/tableShow.vue
+++ src/views/report/gameBetJump/tableShow.vue
@@ -1,17 +1,15 @@
 <template>
   <el-dialog :visible.sync="open" title="DETAILS" append-to-body>
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column property="gameplamid" label="游戏编号id" header-align="center" align="center"/>
-      <el-table-column property="gameagent" label="平台编号" header-align="center" align="center"/>
-      <el-table-column property="agentchild" label="子平台编号" header-align="center" align="center"/>
+      <el-table-column property="gameplamid" label="有效下注" header-align="center" align="center"/>
+      <el-table-column property="gameagent" label="盈利" header-align="center" align="center"/>
+      <el-table-column property="agentchild" label="会员ID" header-align="center" align="center"/>
     </el-table>
     <pagination
-      v-show="queryParam.total > 0"
-      :total="queryParam.total"
-      :page-sizes="[10,20,50,100]"
-      :page.sync="queryParam.pageNum"
-      :limit.sync="queryParam.pageSize"
-      layout="prev, pager, next"
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
       @pagination="list"
     />
   </el-dialog>
@@ -27,7 +25,11 @@ export default {
     return {
       tableData: [],
       open: false,
-      queryParam: {}
+      total:0,
+      queryParams: {
+        pageNum:1,
+        pageSize:10,
+      }
     }
   },
   methods: {
@@ -37,7 +39,7 @@ export default {
     },
     list(){
       listByGamePepole(this.queryParam).then((res) =>{
-        this.queryParam.total = res.total
+        this.total = res.total
         this.tableData = res.rows;
       })
       this.open = true;

304af0f48d1d8512e5006457e0d20deab733c888
mengjun <mengjun8877@gmail.com>
2024-04-11 22:23:18 +0800

    1

--- src/views/activity/activityManage/activityWithdrawCashBack/index.vue
+++ src/views/activity/activityManage/activityWithdrawCashBack/index.vue
@@ -48,7 +48,8 @@
     />
     <el-dialog :visible.sync="open" :title="title" width="500px" append-to-body>
       <el-form ref="form" :model="form" label-width="100px">
-        <el-form-item v-for="item in Object.values(QUERY_TABLE_COLUMNS)" :label="item.LABEL" :prop="item.PROP">
+        <el-form-item v-for="item in Object.values(QUERY_TABLE_COLUMNS)"
+                      v-show="item.PROP !== QUERY_TABLE_COLUMNS.STATUS.PROP" :label="item.LABEL" :prop="item.PROP">
           <el-select v-if="item.PROP === QUERY_TABLE_COLUMNS.BANK_CODE.PROP" v-model="form.bankCode">
             <el-option
               v-for="bank in bankCodeList"
@@ -56,8 +57,7 @@
               :label="bank.code"
               :value="bank.code"/>
           </el-select>
-          <el-input v-else v-model="form[item.PROP]" type="number"
-                    v-show="item.PROP !== QUERY_TABLE_COLUMNS.STATUS.PROP"/>
+          <el-input v-else v-model="form[item.PROP]" type="number" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">

b96a43377e36803c0c920e97d6f12dcb05f48f7c
rajesh <rkyadavsoft@gmail.com>
2024-04-12 12:04:59 +0800

    1

--- src/views/server/sms/index.vue
+++ src/views/server/sms/index.vue
@@ -152,22 +152,22 @@
         <el-form-item label="appAccess" prop="appAccess">
           <el-input v-model="form.appAccess" placeholder="请输入appAccess"/>
         </el-form-item>
-        <el-form-item label="签名" prop="signature">
+        <el-form-item label="签名" prop="signature" v-if="form.provider != 5">
           <el-input v-model="form.signature" placeholder="请输入签名"/>
         </el-form-item>
         <el-form-item label="分类的密钥" prop="classificationKey" v-if="form.provider == 4">
           <el-input v-model="form.classificationKey" placeholder="请输入分类的密钥"/>
         </el-form-item>
-        <el-form-item label="模板" prop="template">
+        <el-form-item label="模板" prop="template" v-if="form.provider != 5">
           <el-input v-model="form.template" placeholder="请输入模板"/>
         </el-form-item>
-        <el-form-item label="地区" prop="region" v-if="form.provider != 3 && form.provider != 4">
+        <el-form-item label="地区" prop="region" v-if="form.provider != 3 && form.provider != 4 && form.provider != 5">
           <el-input v-model="form.region" placeholder="请输入地区"/>
         </el-form-item>
-        <el-form-item label="smsSdkAppid" prop="smsSdkAppid" v-if="form.provider == 0">
+        <el-form-item label="smsSdkAppid" prop="smsSdkAppid" v-if="form.provider == 0 && form.provider != 5">
           <el-input v-model="form.smsSdkAppid" placeholder="请输入smsSdkAppid"/>
         </el-form-item>
-        <el-form-item label="请求域名" prop="endpoint" v-if="form.provider == 3 || form.provider == 4">
+        <el-form-item label="请求域名" prop="endpoint" v-if="form.provider == 3 || form.provider == 4 || form.provider == 5">
           <el-input v-model="form.endpoint" placeholder="请输入请求域名"/>
         </el-form-item>
       </el-form>
@@ -231,6 +231,10 @@ export default {
         {
           name: "云极",
           value: 4
+        },
+        {
+          name: "短信宝",
+          value: 5
         }],
 
       // 状态字典
